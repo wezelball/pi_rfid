7e1a945a0626c5ff8e24be84a8aead2c5dc2f20d
Michael Shires <mshires1@gmail.com>
2015-01-19 16:13:16 -0500

    Added more commands, can now detrect and read tags when type set to T55xx (cards used T5567)

--- rfidtest.py
+++ rfidtest.py
@@ -73,17 +73,27 @@ def main():
 	serialPort.flushInput()
 	serialPort.flushOutput()
 	
+	# Write the main screen
 	stdscr.clear()
 	stdscr.addstr(0,0, "Commands")
 	stdscr.addstr(2,0, "(0) Report version number")
 	stdscr.addstr(3,0, "(1) Measure operating frequency")
 	stdscr.addstr(4,0, "(2) Toggle reader status (active/inactive)")
 	stdscr.addstr(5,0, "(3) Select tag type")
+	stdscr.addstr(6,0, "(4) Locate transdponder tag")
+	stdscr.addstr(7,0, "(5) Read standard data")
+	stdscr.addstr(8,0, "(6) Read atag data")
 
-	stdscr.addstr(8,0, "(9) Exit program")
-
+	stdscr.addstr(11,0, "(9) Exit program")
+	
+	# The options dictionary contains a mapping of command numbers
+	# to commands.  Command number >= 50 are not user commands
+	# but are sent by the application after some preprocessing
 	options = { 0: "VER\r",		# firmware version
 				1: "MOF\r",		# measure operating frequency
+				4: "LTG\r",		# locate transponder
+				5: "RSD\r",		# read standard data
+				6: "RAT\r",		# read atag data
 				50: "SRA\r",	# set reader active
 				51: "SRD\r",	# set reader deactive
 				52: "ST0\r",	# EM4100 tag selecteced (volatile)
@@ -124,30 +134,34 @@ def main():
 			locy = 5
 			
 			# Create a sub-window for the tag type
-			sub_cornerX = 10
-			sub_cornerY = 10
+			sub_cornerX = 18
+			sub_cornerY = 13
 			sub_height = 5
-			sub_width = 25
+			sub_width = 20
 			subwin = curses.newwin(sub_height, sub_width, sub_cornerY, \
 				sub_cornerX)
 			subwin.addstr(0,0, "(1) - EM4100")
 			subwin.addstr(1,0, "(2) - T55xx")
 			subwin.addstr(2,0, "(3) - FDX-B/HDX")
 			subwin.addstr(3,0, "(4) - TIRIS")
-			subwin.addstr(3,0, "(5) - EM4205")
+			subwin.addstr(4,0, "(5) - EM4205")
 			subwin.refresh()
 
 			# This selection is for the tag type subwindow
+			# Note the that the screenData length is 9 characters,
+			# not including carriage return.  This is to insure that
+			# the data output areas are wiped clean.  This is a real
+			# ghetto way of doing this, and must be improved
 			c = stdscr.getch()
 			if c == ord('1'):
 				command = 52
-				screenData = "EM4100"
+				screenData = "EM4100   "
 				del subwin
 				stdscr.touchwin()
 				stdscr.refresh()
 			if c == ord('2'):
 				command = 53
-				screenData = "T55xx"
+				screenData = "T55xx    "
 				del subwin
 				stdscr.touchwin()
 				stdscr.refresh()
@@ -159,17 +173,32 @@ def main():
 				stdscr.refresh()
 			if c == ord('4'):
 				command = 55
-				screenData = "TIRIS"
+				screenData = "TIRIS    "
 				del subwin
 				stdscr.touchwin()
 				stdscr.refresh()
 			if c == ord('5'):
 				command = 56
-				screenData = "EM4205"
+				screenData = "EM4205   "
 				del subwin
 				stdscr.touchwin()
 				stdscr.refresh()
-					
+				
+		elif c == ord('4'):	# locate transponder
+			command = 4
+			locx = 50
+			locy = 6
+			#screenData = ''
+		elif c == ord('5'):	# read standard data
+			command = 5
+			locx = 50
+			locy = 7
+			#screenData = ''
+		elif c == ord('6'):	# read atag data
+			command = 6
+			locx = 50
+			locy = 8
+			screenData = ''			
 		elif c == ord('9'):
 			# Exit cleanly
 			getOut()
@@ -181,6 +210,19 @@ def main():
 		# Force screen update NOW
 		stdscr.refresh()
 		inStr = sendCommand(outStr)
+		# Need to check here and modify screenData based on
+		# response from reader
+		if command == 4 and inStr == "?1\r":
+			screenData = "absent"
+		elif command == 4 and inStr == "OK\r":
+			screenData = "present"
+		elif command == 5 and inStr == "?1\r":
+			screenData = "none"
+		elif command == 5 and inStr <> "?1\r":
+			screenData = inStr
+		elif command == 6 and inStr <> "?1\r":
+			screenData = inStr
+		
 		# What is actually being printed in the screen
 		if screenData == '':
 			stdscr.addstr(locy, locx, inStr, curses.A_REVERSE)
